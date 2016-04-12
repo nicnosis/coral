79018b5cc682942cd69a2362d8fd386c3d0e2b41
mtgNic <nic.jsb@gmail.com>
2016-04-12 11:44:16 -0400

    working on d3 map

--- js/render-maps.js
+++ js/render-maps.js
@@ -1,7 +1,109 @@
-// initialize the map
-var map = L.map('map').setView([42.35, -71.08], 13);
+var mapObj = {
+    "path" : {
+        "csv" : "data/reef-locations.csv",
+        "topo" : "data/world-topo.topojson"
+    },
+    "dimensions" : {
+        "margin" : { top : 20,  right: 20,  bottom: 30,  left: 50 },
+        "width" : 1100,
+        "height" : 525
+    }
+}
 
-// load a tile layer
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+function renderMap (obj) {
+    //Map dimensions (in pixels)
+    var width = obj.dimensions.width,
+        height = obj.dimensions.height;
+
+//Map projection
+    var projection = d3.geo.equirectangular()
+        .scale(175)
+        .center([0,0]) //projection center
+        .translate([width / 2, height / 2]) //translate to center the map in view
+
+//Generate paths based on projection
+    var path = d3.geo.path().projection(projection);
+
+//Create an SVG
+    var svg = d3.select("#map").append("svg").attr("width", width).attr("height", height);
+
+//Group for the map features
+    var features = svg.append("g").attr("class", "features");
+
+//Create a tooltip, hidden at the start
+    var tooltip = d3.select("body").append("div").attr("class", "world-tooltip");
+
+//Create zoom/pan listener
+//Change [1,Infinity] to adjust the min/max zoom scale
+    var zoom = d3.behavior.zoom()
+        .scaleExtent([1, Infinity])
+        .on("zoom", zoomed);
+
+    svg.call(zoom);
+
+// Color scale
+//     var threshold = d3.scale.threshold()
+//         .domain(obj.domain)
+//         .range(d3.range(obj.numQuantiles).map(function (i) { return "q" + i + "-" + obj.numQuantiles; }));
+
+    var valueByCountryCode = d3.map();
+
+    var q = d3_queue.queue();
+    q
+        .defer(d3.json, obj.path.topo)
+        .defer(d3.csv, obj.path.csv, function (d) { valueByCountryCode.set(d.CountryCode, +d[obj.mapParam]) })
+        .await(ready);
+
+    // The two defer calls will not be executed until ready() is
+    function ready(error, geodata) {
+        if (error) throw error;
+
+        //Create a path for each map feature in the data
+        features.selectAll("path")
+            .data(topojson.feature(geodata,geodata.objects.countries).features) //generate features from TopoJSON
+            .enter()
+            .append("path")
+            .attr("fill", "#666")
+            .attr("d",path)
+            .on("mouseover",showTooltip)
+            .on("mousemove",moveTooltip)
+            .on("mouseout",hideTooltip)
+            .on("click",clicked);
+    }
+
+    /***** MAP CONTROLS GO HERE ************/
+
+// Add optional onClick events for features here
+// d.properties contains the attributes (e.g. d.properties.name, d.properties.population)
+    function clicked(d,i) { }
+
+//Update map on zoom/pan
+    function zoomed() {
+        features.attr("transform", "translate(" + zoom.translate() + ")scale(" + zoom.scale() + ")")
+            .selectAll("path").style("stroke-width", 1 / zoom.scale() + "px" );
+    }
+
+//Position of the tooltip relative to the cursor
+    var tooltipOffset = {x: 5, y: -25};
+
+//Create a tooltip, hidden at the start
+    function showTooltip(d) {
+        moveTooltip();
+
+        tooltip.style("display","block")
+            .text("");
+    }
+
+//Move the tooltip to track the mouse
+    function moveTooltip() {
+        tooltip.style("top",(d3.event.pageY + tooltipOffset.y) + "px")
+            .style("left",(d3.event.pageX + tooltipOffset.x) + "px");
+
+    }
+
+//Create a tooltip, hidden at the start
+    function hideTooltip() {
+        tooltip.style("display","none");
+    }
+
+}
